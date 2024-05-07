a200ec548a859005d8a5a39823c42c718b12b94c
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-05-07 15:28:04 -0300

    Pegando informações do banco de dados

--- teste.py
+++ teste.py
@@ -11,6 +11,27 @@ class Usuario(BaseModel):
     senha: str
 
 
-@app.post('/usuarios')
+usuarios = [
+    Usuario(id=1, nome='Guilherme', senha='senhadoGuilherme'),
+    Usuario(id=2, nome='Andressa', senha='senhadaAndressa'),
+    Usuario(id=3, nome='Liz', senha='senhadaLiz'),
+]
+
+
+@app.get('/usuario/{id}')
+def main(id: int):
+    for usuario in usuarios:
+        if usuario.id == id:
+            return usuario
+    return 'Esse usuário não existe'
+
+
+@app.get('/listar_usuarios')
+def listar():
+    return usuarios
+
+
+@app.post('/cadastrar_usuario')
 def main(usuario: Usuario):
-    return usuario  
+    usuarios.append(usuario)
+    return 'Usuário cadastrado'
